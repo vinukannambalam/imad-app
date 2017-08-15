05871aced44cb6aa83dc8fe4e122531d18ac9506
Vinayachandran <30824921+vinukannambalam@users.noreply.github.com>
2017-08-15 12:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,10 +78,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var articleName = req.params.articleName;
+
 app.get('/:articleName', function(req,res){
     // articleName == article-one
     // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
  
