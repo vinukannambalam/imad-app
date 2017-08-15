cd51faa4d558b934fc801000c28b5454926b89ab
Vinayachandran <30824921+vinukannambalam@users.noreply.github.com>
2017-08-15 17:35:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function () {
    
 };
 //Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //Make a request to the server and send the name
@@ -53,6 +52,8 @@ submit.onclick = function () {
        }
 };
    //Make the request
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
    request.open('GET', 'http://vinayachandranv.imad.hasura-app.io/submit-name?name=' + name, true);
    request.send(null);
 };
