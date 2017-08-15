49fdcc79cf335f66886f3c3f9ae41f558a713da5
Vinayachandran <30824921+vinukannambalam@users.noreply.github.com>
2017-08-15 18:42:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ submit.onclick = function () {
                 var list = '';
                 for(var i=0; i<names.length; i++) {
                     list += '<li>' + names[i] + '</li>';
-                    alert('Hi');
+                    alert(Hi);
                 }
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
